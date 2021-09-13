09d26e990c6dd65c27ca065d6c3aa1a77278b409
JonaPierini <jonapierini@gmail.com>
2021-09-13 19:21:24 -0300

    13.9

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+// Carrousel Uno
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel1__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel1__indicadores',
+		arrows: {
+			prev: '.carousel1__anterior',
+			next: '.carousel1__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+});
+
+// Carousel Dos
 let items = document.querySelectorAll('.carousel .carousel-item')
 
 items.forEach((el) => {
